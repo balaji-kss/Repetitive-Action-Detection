ec81207c22f8dbcabbd1bbe5255eddcc71025b4a
balaji-sundar <kss.balaji1@gmail.com>
2023-04-01 10:16:51 -0400

    added gt - vis data

--- main.py
+++ main.py
@@ -4,20 +4,55 @@ import cv2
 import matplotlib.pyplot as plt
 import math
 
-def display_skeleton(data, image):
+def display_skeleton(image, data):
+
 	for i in range(data.shape[0]):
 		if (math.isnan(data[i][0]) or math.isnan(data[i][1])):
 			continue
-		image = cv2.circle(image, (int(data[i][0]), int(data[i][1])), 3, (255, 255, 255), 2)
+		image = cv2.circle(image, (int(data[i][0]), int(data[i][1])), 3, (0, 0, 255), 2)
 
 	return image
 
+def display_label(image, label, frameid):
+
+	cv2.putText(
+		img = image,
+		text = str(frameid) + " : " + str(label),
+		org = (40, 40),
+		fontFace = cv2.FONT_HERSHEY_DUPLEX,
+		fontScale = 1.0,
+		color = (0, 0, 255),
+		thickness = 1
+	)
+
+	return image
+
+def vis_loader(dataset):
+
+	for frameid in range(len(dataset)):
+		
+		sample = dataset[frameid]
+		img = sample["image"]
+
+		if train:
+			label = sample["label"]
+		else:
+			label = 0
+
+		joints = sample["joints"]
+
+		disp_img = display_skeleton(img, joints)
+		display_label(disp_img, label, frameid)
+
+		cv2.imshow("Test", disp_img)
+
+		cv2.waitKey(-1)
+
 if __name__ == "__main__":
-	dataset = PoseDataset("hard_data/folding/clip_1")
+	
+	train = True
+	dataset = PoseDataset("simple_data/lifting_1/clip_1", train=train)
 
 	loader = DataLoader(dataset)
-	for sample in loader:
-		img = sample["image"].numpy()
-		cv2.imshow("Test", display_skeleton(sample["joints"].numpy()[0], img[0]))
 
-		cv2.waitKey(0)
+	vis_loader(dataset)
--- pose_dataset.py
+++ pose_dataset.py
@@ -12,15 +12,48 @@ class PoseDataset(Dataset):
         data_prefix (str): Path to a directory where pose data is held
     """
 
-    def __init__(self, data_prefix):
+    def __init__(self, data_prefix, train):
+        
+        self.train = train
         self.video_file = f"{data_prefix}/video.mp4"
         self.joints_2d_file = f"{data_prefix}/2d.csv"
         self.ground_truth_file = f"{data_prefix}/gt.csv"
-
+        
         self.joints_2d = self.load_joint_data_2d()
+        self.video = mmcv.VideoReader(self.video_file)
 
+        assert len(self.joints_2d) == len(self.video)
+        self.num_frames = len(self.video)
+
+        if self.train:
+            self.labels = self.load_gt()
+
+    def load_gt(self):
+        
+        end_time_stamps = []
+        csvfile = open(self.ground_truth_file, newline='')
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i = 0
+        prev_ts = 0
+        for row in reader:
+            int_prev_ts = int(round(prev_ts))
+            end_time_stamps.append(int_prev_ts)
+            print('before prev_ts ', prev_ts)
+            prev_ts = float(row[0]) * self.video.fps + prev_ts
+            print('prev_ts ', prev_ts, self.video.fps, float(row[0]))
+
+        int_prev_ts = int(round(prev_ts))
+        end_time_stamps.append(int_prev_ts)
+        csvfile.close()
+        print('end_time_stamps ', end_time_stamps)
+        tot_time_stamps = [0] * self.num_frames
+        tot_time_stamps_np = np.array(tot_time_stamps)
+        tot_time_stamps_np[end_time_stamps] = 1
+
+        return np.array(tot_time_stamps_np)
 
     def load_joint_data_2d(self):
+
         data = []
         csvfile = open(self.joints_2d_file, newline='')
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -36,6 +69,7 @@ class PoseDataset(Dataset):
             data.append(row_data)
             i += 1
         csvfile.close()
+
         return np.array(data)
 
     def __len__(self):
@@ -44,10 +78,19 @@ class PoseDataset(Dataset):
 
     def __getitem__(self, idx):
         """Get a sample with given index."""
-        video = mmcv.VideoReader(self.video_file)
-        frame = video[idx]     
-        return {
-            "joints": self.joints_2d[idx],
-            "image": frame
-        }
+        
+        frame = self.video[idx]
+
+        if self.train:
+            label = self.labels[idx]
+            return {
+                "joints": self.joints_2d[idx],
+                "image": frame,
+                "label": label
+            }
+        else:
+            return {
+                "joints": self.joints_2d[idx],
+                "image": frame
+            }
 
