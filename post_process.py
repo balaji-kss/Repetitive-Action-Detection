468158fbe6087f10e51802289d08988be7b00231
balaji-sundar <kss.balaji1@gmail.com>
2023-04-03 12:19:35 -0400

    post process - WIP

--- post_process.py
+++ post_process.py
@@ -2,39 +2,6 @@ import cv2
 import numpy as np
 import csv
 
-def read_lst(lst_path):
-
-    with open(lst_path, 'r') as ann_file:
-        lines = ann_file.readlines()
-        words = lines[0].rstrip().strip('\n').split(',')        
-        
-    return np.array([float(word) for word in words])
-
-def get_durations(frame_confs, thresh):
-
-    print('frame_confs ', frame_confs, frame_confs.shape)
-
-
-def read_video(video_path):
-
-    cap = cv2.VideoCapture(video_path)    
-
-    frames = []
-    i = 0
-    while True:
-
-        ret, frame = cap.read()
-        if ret is False: break
-    
-        i += 1
-
-        # if i < 7300: continue
-        # if i%3 != 0:continue
-
-        # frame = display_label(frame, i)
-        cv2.imshow('frame ', frame)
-        cv2.waitKey(-1)
-
 def display_label(image, frameid):
 
 	cv2.putText(
@@ -69,27 +36,68 @@ def load_gt(csv_file):
 
     return end_time_stamps
         
+def read_lst(lst_path):
+
+    with open(lst_path, 'r') as ann_file:
+        lines = ann_file.readlines()
+        words = lines[0].rstrip().strip('\n').split(',')        
+        
+    return np.array([float(word) for word in words])
+
+def get_durations(frame_confs, thresh):
+
+    print('frame_confs ', frame_confs, frame_confs.shape)
+
+def get_mov_avg(smooth_window, len_sw, cur_pred):
+
+    smooth_window.append(cur_pred)
+    if len(smooth_window) > len_sw:
+        smooth_window.pop(0)
+    
+    avg = np.sum(smooth_window) / len(smooth_window)
+
+    return round(avg, 3), smooth_window
+
+def get_mov_avgs(len_sw, preds):
+
+    smooth_win, pred_avgs = [], []
+    for pred in preds:
+        pred_avg, smooth_win = get_mov_avg(smooth_win, len_sw, pred)
+        pred_avgs.append(pred_avg)
+
+    return pred_avgs
+
+def run(video_path, lst_path):
+
+    cap = cv2.VideoCapture(video_path)    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print('fps ', fps)
+    len_sw = fps // 3
+
+    frame_preds = read_lst(lst_path)
+    get_durations(frame_preds, thresh)
+    pred_avgs = get_mov_avgs(len_sw, frame_preds)
+
+    i = 0
+    
+    while True:
+
+        ret, frame = cap.read()
+        if ret is False: break
+    
+        pred, pred_avg = frame_preds[i], pred_avgs[i]
+
+        cv2.imshow('frame ', frame)
+        print('pred ', pred, ' pred avg ', pred_avg)
+        cv2.waitKey(-1)
+
+        i += 1
 
 if __name__ == "__main__":
 
-    root_dir = '/home/balajisundar/Documents/US/NEU/Full-time/Company/TuMeke/tumeke_takehome_final/tumeke_takehome_final/results_new/'
-    video_path = root_dir + 'folding_clip_2.mp4'
-    lst_path = root_dir + 'folding_clip_2.txt'
+    root_dir = './results/'
+    video_path = root_dir + 'lifting_1_clip_2.mp4'
+    lst_path = root_dir + 'lifting_1_clip_2.txt'
     thresh = 0.5
 
-    frame_confs = read_lst(lst_path)
-    get_durations(frame_confs, thresh)
-
-    rvideo_path = '/home/balajisundar/Documents/US/NEU/Full-time/Company/TuMeke/tumeke_takehome_final/tumeke_takehome_final/results/kontoor_clip_2.mp4'
-
-    # rvideo_path = './simple_data/lifting_3/clip_1/video.mp4'
-    # csv_file = './simple_data/lifting_3/clip_1/gt.csv'
-
-    # rvideo_path = './hard_data/kontoor/clip_1/video.mp4'
-    # csv_file = './hard_data/kontoor/clip_1/gt.csv'
-    # ets = load_gt(csv_file)
-    # ets = [0] + ets
-    # ets_np = np.array(ets)
-    # print('diff ', ets_np[1:] - ets_np[:-1])
-    # print('ets ', ets)
-    # read_video(rvideo_path)
+    run(video_path, lst_path)
